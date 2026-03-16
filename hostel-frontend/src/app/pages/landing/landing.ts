fc8b7f7bc21199cf03e25fa38a45ad324d718b9c
Purnachandra Mallick <purnachandram674@gmail.com>
2026-03-16 11:11:53 +0530

    Update the Styling in the landing page , login page and register page

--- hostel-frontend/src/app/pages/landing/landing.ts
+++ hostel-frontend/src/app/pages/landing/landing.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,35 @@ import { RouterLink } from '@angular/router';
   templateUrl: './landing.html',
   styleUrls: ['./landing.css']
 })
-export class LandingComponent {}
+export class LandingComponent implements OnInit {
+
+  ngOnInit() {
+    this.animateCounter("studentsCount", 500);
+    this.animateCounter("hostelsCount", 20);
+    this.animateCounter("roomsCount", 1200);
+  }
+
+  animateCounter(id: string, target: number) {
+
+    const element = document.getElementById(id);
+
+    if (!element) return;
+
+    let count = 0;
+    const speed = target / 80;
+
+    const counter = setInterval(() => {
+
+      count += speed;
+
+      if (count >= target) {
+        element.innerText = target + "+";
+        clearInterval(counter);
+      } else {
+        element.innerText = Math.floor(count).toString();
+      }
+
+    }, 20);
+  }
+
+}
